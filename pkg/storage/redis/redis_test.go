e38dd2491bae4eeb228bf10b343fe4a675eeeab1
vkuksa <kuksa.vladyslav@gmail.com>
2023-05-23 22:29:05 +0300

    Fixed error with redis dublication

--- pkg/storage/redis/redis_test.go
+++ pkg/storage/redis/redis_test.go
@@ -1,6 +1,7 @@
 package redis_test
 
 import (
+	"os"
 	"testing"
 
 	"github.com/vkuksa/shortly/pkg/storage/redis"
@@ -8,12 +9,25 @@ import (
 )
 
 var (
-	options = redis.Options{
-		Address:  "localhost:6379",
-		Password: "",
+	RedisHost = "localhost"
+	RedisPort = "6379"
+	options   = redis.Options{
+		Address:  RedisHost,
+		Password: RedisPort,
 		DB:       15,
 	}
 )
+
+func init() {
+	host, exists := os.LookupEnv("REDIS_HOST")
+	if exists {
+		RedisHost = host
+	}
+	port, exists := os.LookupEnv("REDIS_PORT")
+	if exists {
+		RedisPort = port
+	}
+}
 
 func TestClose(t *testing.T) {
 	s, _ := test.MustCreateStorage[int](t, "redis", options)
